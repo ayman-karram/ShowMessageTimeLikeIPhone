84c6c39f816a4bc2d67848ea46cc64e5e5fadd09
ayman karram <ayman.karram@gmail.com>
2015-10-02 19:58:17 +0200

    Initial Commit

---
+++ ShowMessagesDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowMessagesDate
+//
+//  Created by Ayman  on 10/2/15.
+//  Copyright © 2015 ayman mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
