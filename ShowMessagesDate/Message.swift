f7b6bd6b8c8ad6298c4ec346e2b33345f95c1e1b
ayman karram <ayman.karram@gmail.com>
2015-11-20 19:21:38 +0200

    first commit

---
+++ ShowMessagesDate/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  ShowMessagesDate
+//
+//  Created by Ayman  on 11/20/15.
+//  Copyright © 2015 ayman mohamed. All rights reserved.
+//
+
+import Foundation
---
+++ ShowMessagesDate/ShowMessagesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ShowMessagesViewController.swift
+//  ShowMessagesDate
+//
+//  Created by Ayman  on 10/2/15.
+//  Copyright © 2015 ayman mohamed. All rights reserved.
+//
+
+import Foundation
